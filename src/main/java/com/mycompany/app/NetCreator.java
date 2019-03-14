6a139f42d0f401c68643ee612cab7d66184886bf
WIktor <wiktor-rup@wp.pl>
2019-03-14 15:24:25 +0100

    Added Setting class and test

--- src/main/java/com/mycompany/app/NetCreator.java
+++ src/main/java/com/mycompany/app/NetCreator.java
@@ -5,18 +5,23 @@ package com.mycompany.app;
  */
 class NetCreator {
 
-    private final int row;
-    private final int column;
+    //    private final int row;
+//    private final int column;
+    private Settings settings;
 
-    NetCreator(int row, int column) {
-        this.row = row;
-        this.column = column;
+//    NetCreator(int row, int column) {
+//        this.row = row;
+//        this.column = column;
+//    }
+
+    NetCreator(Settings settings) {
+        this.settings = settings;
     }
 
     Net createNet() {
-        Field[][] fields = new Field[row][column];
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < column; j++) {
+        Field[][] fields = new Field[settings.getRow()][settings.getColumn()];
+        for (int i = 0; i < settings.getRow(); i++) {
+            for (int j = 0; j < settings.getColumn(); j++) {
                 fields[i][j] = new Field(Sign.EMPTY);
             }
         }
